ce091494441756e3faf399bf330efe17dc84b69d
Ye-Egor <Shadrin-yegor29@yandex.ru>
2023-09-22 21:07:57 +0500

    Task2

---
+++ Tyuiu.ShadrinEA.Sprint1.Task0.V21.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ShadrinEA.Sprint1.Task2.V21.Lib;
+
+namespace Tyuiu.ShadrinEA.Sprint1.Task2.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            int x = 2;
+            int y = 3;
+
+            var res = ds.CalculateRectangleSquare(x, y);
+            Assert.AreEqual(6, res);
+        }
+    }
+}
---
+++ Tyuiu.ShadrinEA.Sprint1.Task2.V0/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShadrinEA.Sprint1.Task2.V21.Lib;
+
+namespace Tyuiu.ShadrinEA.Sprint1.Task2.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Шадрин Е.А | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Шадрин Егор Андреевич | ИСТНб-23-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет площадь прямоугольника и печатает результат на экране.        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x; int y;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Площадь прямоугольника = x * y = {ds.CalculateRectangleSquare(x, y)}");
+            Console.ReadKey();
+
+        }
+    }
+}
